c883debced4892bac15209cb398ec4493b75c92e
Michal Mocnak <michal@marigan.net>
2022-11-01 11:30:07 +0100

    stats extension fix, version v0.4.5

--- lib/narra/extensions/stats.rb
+++ lib/narra/extensions/stats.rb
@@ -2,6 +2,8 @@
 # Copyright: (c) 2021, Narra Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+require 'json'
+
 module Narra
   module Extensions
     module Stats
@@ -16,22 +18,18 @@ module Narra
         # get public meta
         meta = get_meta(name: 'stats')
         # return
-        meta.nil? ? { items: 0 } : meta.value
-      end
-
-      def stats=(stats)
-        self.update_meta(name: stats, value: stats)
+        meta.nil? ? { 'items': 0 } : JSON.parse(meta.value)
       end
 
       # update stats field
       def update_stats
-        self.stats = { items: self.items.count }
+        self.update_meta(name: 'stats', value: { items: self.items.count }.to_json)
       end
 
       protected
 
       def narra_stats_initialize
-        self.add_meta(name: 'stats', value: { items: 0 }, hidden: false, public: true)
+        self.add_meta(name: 'stats', value: { items: 0 }.to_json, hidden: false, public: true)
       end
     end
   end
